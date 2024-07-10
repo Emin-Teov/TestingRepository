bde8a6a29bb7d73906868855ff0cd78d67c6fdde
Emin-Teov <e1000.tavakkulov@gmail.com>
2024-07-10 10:45:22 +0400

    set logo

--- resources/view/head.php
+++ resources/view/head.php
@@ -4,6 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="resources/Logo.png" type="image/x-icon"/>
         <link rel="stylesheet" href="resources/css/main.css">
         <title>Testing</title>
     </head>
